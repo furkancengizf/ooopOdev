2d6fe13f6302d070475613fb59ae16a80e0ea166
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-04-27 18:50:42 +0300

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,15 @@
+package Homework;
+
+public class Course(int id , String name , String instructor){
+  this.id = ;
+  this.name = name;
+  this.instructor = instructor;
+
+}
+  int id;
+  String name;
+  String instructor;
+  
+}
+ 
+ 
