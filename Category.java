b0bb6170d1bcaf54e3a7caf1c6c504f30be5c4f8
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-04-27 18:49:52 +0300

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,14 @@
+package Homework;
+
+public class Category(int id , String name ){
+  
+  this.id = id;
+  this.name = name;
+  System.out.println("Kategori:" name "\nKategori Eklendi");
+  
+   int id;
+   string name;
+   
+   }
+   
+}
